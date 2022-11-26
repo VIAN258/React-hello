d6459c027be2e5a425a0120081f78f0d237378c4
VIAN258 <angelmorales891@gmail.com>
2022-11-26 00:52:36 +0000

    falta footer

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+//include images into your bundle
+//import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Jumbotron = () => {
+	return (
+        <div class="jumbotron">
+          <div class="container">
+            <h1>Lorem ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p><a class="btn btn-primary btn-lg" role="button">Leer más</a></p>
+          </div>
+        </div>
+	);
+};
+
+export default Jumbotron;
+
+
+
+
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Footer from "./component/footer.jsx";
+
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render([<Home/>, <Footer/>], document.querySelector("#app"));
